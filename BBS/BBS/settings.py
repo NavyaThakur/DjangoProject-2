61a946ad9377b01abcc2505c9017674a5728aa3e
Navya Thakur <navya250801@gmail.com>
2021-11-06 20:52:03 +0530

    Update settings.py

--- BBS/BBS/settings.py
+++ BBS/BBS/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'BBS.urls'
@@ -126,3 +127,4 @@ STATICFILES_DIRS =[
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
